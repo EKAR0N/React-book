b48887c20f7eba07dd72a7224cb25852ea5d2140
dever <alsrbtkfkd7@gmail.com>
2018-10-26 22:35:51 +0900

    10/26 config eslint, prettier

---
+++ styling-react-2/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SassComponent from './SassComponent';
+
+const a = () => 2;
+
+class App extends Component {
+  render() {
+    return <SassComponent />;
+  }
+}
+
+export default App;
