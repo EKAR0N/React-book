f32ede8ac1c62fcdb94010aa56db3ba1a5b71a3d
dever <alsrbtkfkd7@gmail.com>
2018-11-07 22:29:45 +0900

    clone repo

--- todo-list/src/components/TodoInput/TodoInput.js
+++ todo-list/src/components/TodoInput/TodoInput.js
@@ -1,29 +1,28 @@
 import React from 'react';
+import styles from './TodoInput.scss';
 import classNames from 'classnames/bind';
-import styles from './TodoInput.module.scss';
 
 const cx = classNames.bind(styles);
 
-// input과 버튼이 함게 있는 컴포넌트.
-/* value input값
-   onChange: input 변경 이벤트
-   onInsert: 추가 버튼 클릭 이벤트
+// 인풋과 버튼이 함께 있는 컴포넌트입니다. 
+/*
+  value: 인풋 값
+  onChange: 인풋 변경 이벤트
+  onInsert: 추가버튼 클릭 이벤트
 */
-
-const TodoInput = ({ value, onChange, onInsert }) => {
-  // Enter키가 눌리면 onInsert 실행
-  const handleKeyPress = e => {
-    if (e.key === 'Enter') {
+const TodoInput = ({value, onChange, onInsert}) => {
+  
+  // 엔터키가 눌리면 onInsert 를 실행합니다.
+  const handleKeyPress = (e) => {
+    if(e.key === 'Enter') {
       onInsert();
     }
-  };
+  }
 
   return (
     <div className={cx('todo-input')}>
-      <input onChange={onChange} value={value} onKeyPress={handleKeyPress} />
-      <div className={cx('add-button')} onClick={onInsert}>
-        추가
-      </div>
+      <input onChange={onChange} value={value} onKeyPress={handleKeyPress}/>
+      <div className={cx('add-button')} onClick={onInsert}>추가</div>
     </div>
   );
 };
