af3a67e2e31dd2c097d9fc0cab5079061fe25152
dever <alsrbtkfkd7@gmail.com>
2018-11-10 22:39:48 +0900

    eslint, prettier config

--- nasa-zpod/config/webpack.config.dev.js
+++ nasa-zpod/config/webpack.config.dev.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable */
 
 const fs = require('fs');
 const path = require('path');
@@ -314,10 +314,13 @@ module.exports = {
                 importLoaders: 2,
                 modules: true,
                 getLocalIdent: getCSSModuleLocalIdent,
-              },
-              'sass-loader'
-            ),
-          },
+              }).concat({
+                loader: require.resolve('sass-loader'),
+                options: {
+                  includePaths: [paths.appSrc + '/styles']
+                }
+              })
+            },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
---
+++ nasa-zpod/src/components/ViewerTemplate/ViewerTemplate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './ViewrTemplate.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const ViewrTemplate = ({ viewer, spaceNavigator}) => {
+  return (
+    <div className={cx('viewer-template')}>
+      
+    </div>
+  )
+}
