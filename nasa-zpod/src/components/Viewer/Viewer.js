3d7b6de297324aa76364429c898d1d1d31af43e8
dever <alsrbtkfkd7@gmail.com>
2018-11-13 23:15:09 +0900

    11/13 before api use

--- nasa-zpod/src/components/Viewer/Viewer.js
+++ nasa-zpod/src/components/Viewer/Viewer.js
@@ -1,13 +1,28 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/prop-types */
 import React from 'react';
-import styles from './Viewer.scss';
 import classNames from 'classnames/bind';
+import styles from './Viewer.module.scss';
 
 const cx = classNames.bind(styles);
 
-const Viewer = ({ mediaType, url, loading}) => {
+// eslint-disable-next-line no-unused-vars
+const Viewer = ({ mediaType, url, loading }) => {
   return (
     <div className={cx('viewer')}>
-        
+      {mediaType === 'image' ? (
+        <img
+          onClick={() => window.open(url)}
+          src={url}
+          alt="space"
+        /> /* {url} === props */
+      ) : (
+        <iframe title="space-video" src={url} frameBorder="0" gesture="media" allow="encryoted-media" allowFullScreen />
+      )}
     </div>
-  )
-}
+  );
+};
+
+export default Viewer;
