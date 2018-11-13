8a6b08529dda94b6ba2fc0808b7a64f5a54618a8
dever <alsrbtkfkd7@gmail.com>
2018-11-13 18:08:08 +0900

    plz commit man

--- nasa-zpod/src/components/SpaceNavigator/SpaceNavigator.js
+++ nasa-zpod/src/components/SpaceNavigator/SpaceNavigator.js
@@ -1,13 +1,27 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
-import styles from './SpaceNavigator.scss';
 import classNames from 'classnames/bind';
+import LeftIcon from 'react-icons/lib/md/chevron-left';
+import RightIcon from 'react-icons/lib/md/chevron-right';
+import styles from './SpaceNavigator.module.scss';
 
 const cx = classNames.bind(styles);
 
-const SpaceNavigator = ({ onPrev, onNext }) => {
-  return (
-    <div className={cx('space-navigator')}>
-      
+const SpaceNavigator = ({ onPrev, onNext }) => (
+  <div className={cx('space-navigator')}>
+    <div className={cx('left', 'end')}>
+      <div className={cx('circle')} onClick={onPrev}>
+        <LeftIcon />
+      </div>
     </div>
-  )
-}
+    <div className={cx('right', 'end')}>
+      <div className={cx('circle')} onClick={onNext}>
+        <RightIcon />
+      </div>
+    </div>
+  </div>
+  );
+
+export default SpaceNavigator;
--- nasa-zpod/src/components/ViewerTemplate/ViewerTemplate.js
+++ nasa-zpod/src/components/ViewerTemplate/ViewerTemplate.js
@@ -1,13 +1,25 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable arrow-body-style */
 import React from 'react';
-import styles from './ViewrTemplate.scss';
 import classNames from 'classnames/bind';
+import styles from './ViewerTemplate.module.scss';
 
 const cx = classNames.bind(styles);
 
-const ViewrTemplate = ({ viewer, spaceNavigator}) => {
+const ViewerTemplate = ({ viewer, spaceNavigator /* props 비구조화 할당 */}) => {
   return (
     <div className={cx('viewer-template')}>
-      
+      <header>
+        Astronomy Picture of the Day
+      </header>
+      <div className={cx('viewer-wrapper')}>
+        {viewer} {/* props 사용 */}
+        <div className={cx('space-navigator-wrapper')}>
+          {spaceNavigator} {/* props 사용 */}
+        </div>
+      </div>
     </div>
   )
 }
+
+export default ViewerTemplate;
