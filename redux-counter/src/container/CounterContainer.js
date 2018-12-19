77d211c56dbf704c660d78242840ac12869c2a78
WDever <alsrbtkfkd7@gmail.com>
2018-12-19 22:04:47 +0900

    sub reducers done.

--- redux-counter/src/container/CounterContainer.js
+++ redux-counter/src/container/CounterContainer.js
@@ -25,8 +25,8 @@ export function getRandomColor() {
 }
 
 const mapStateToProps = (state) => ({
-  color: state.color,
-  number: state.number
+  color: state.colorData.color,
+  number: state.numberData.number
 });
 
 // 액션 생성 함수를 이용하여 액션 생성, 이를 dispatch하는 함수를 만들어 props로 연결
