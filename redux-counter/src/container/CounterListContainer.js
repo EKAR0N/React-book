a6f36ed1954346bfe536f204cb149fcbac6e014a
WDever <alsrbtkfkd7@gmail.com>
2018-12-21 08:07:08 +0900

    done

--- redux-counter/src/container/CounterListContainer.js
+++ redux-counter/src/container/CounterListContainer.js
@@ -1,28 +1,7 @@
 import CounterList from '../components/CounterList';
 import * as actions from '../actions';
 import { connect } from "react-redux";
-
-export function getRandomColor() {
-  const colors = [
-    '#495057',
-    '#f03e3e',
-    '#d6336c',
-    '#ae3ec9',
-    '#7048e8',
-    '#4263eb',
-    '#1c7cd6',
-    '#1098ad',
-    '#0ca678',
-    '#37b24d',
-    '#74b816',
-    '#f59f00',
-    '#f76707'
-  ];
-
-  const random = Math.floor(Math.random() * 13);
-
-  return colors[random];
-}
+import getRandomColor from '../lib/getRandomColor';
 
 const mapStateToProps = (state) => ({counters: state.counters});
 
