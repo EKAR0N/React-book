24c74b1266f04fa6633e62462441bf82e7a961e4
WDever <alsrbtkfkd7@gmail.com>
2019-01-09 10:04:28 +0900

    configed eslint prettier scss

--- todo-list2/src/components/App.js
+++ todo-list2/src/components/App.js
@@ -3,29 +3,30 @@ import PageTemplate from './PageTemplate';
 import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
-const initialTodos = new Array(1).fill(0).map(
-  (foo, index) => ({id: index, text: `일정 ${index}`, done: false})
-);
+const initialTodos = new Array(1)
+  .fill(0)
+  .map((foo, index) => ({ id: index, text: `일정 ${index}`, done: false }));
 
 class App extends Component {
   state = {
     input: '', // 인풋의 값
     // 일정 데이터 초기값
     todos: initialTodos,
-  }
+  };
 
   // 일정 데이터 안에 들어가는 id 값
-  id = 1
-  getId = () => {
-    return ++this.id; // 현재 값에서 1을 더한 값을 반환
-  }
+  id = 1;
 
-  handleChange = (e) => {
+  getId = () => {
+    this.id = this.id + 1 // 현재 값에서 1을 더한 값을 반환
+  }       
+
+  handleChange = e => {
     const { value } = e.target;
     this.setState({
-      input: value
+      input: value,
     });
-  }
+  };
 
   // 새 데이터 추가
   handleInsert = () => {
@@ -35,26 +36,26 @@ class App extends Component {
     const newTodo = {
       text: input,
       done: false,
-      id: this.getId()
+      id: this.getId(),
     };
-    
+
     // 배열 안에 새 데이터를 집어넣습니다.
     this.setState({
       todos: [...todos, newTodo],
-      input: ''
+      input: '',
     });
-  }
+  };
 
   // 투두아이템 토글하기
-  handleToggle = (id) => {
+  handleToggle = id => {
     // id 로 배열의 인덱스를 찾습니다.
     const { todos } = this.state;
     const index = todos.findIndex(todo => todo.id === id);
-    
+
     // 찾은 데이터의 done 값을 반전시킵니다.
     const toggled = {
       ...todos[index],
-      done: !todos[index].done
+      done: !todos[index].done,
     };
 
     // slice 를 통하여 우리가 찾은 index 전 후의 데이터들을 복사합니다
@@ -63,12 +64,12 @@ class App extends Component {
       todos: [
         ...todos.slice(0, index),
         toggled,
-        ...todos.slice(index + 1, todos.length)
-      ]
+        ...todos.slice(index + 1, todos.length),
+      ],
     });
-  }
+  };
 
-  handleRemove = (id) => {
+  handleRemove = id => {
     const { todos } = this.state;
     const index = todos.findIndex(todo => todo.id === id);
 
@@ -76,28 +77,31 @@ class App extends Component {
     this.setState({
       todos: [
         ...todos.slice(0, index),
-        ...todos.slice(index + 1, todos.length)
-      ]
+        ...todos.slice(index + 1, todos.length),
+      ],
     });
-  }
+  };
 
   render() {
     const { input, todos } = this.state;
-    const { 
-      handleChange,
-      handleInsert,
-      handleToggle,
-      handleRemove
-    } = this;
+    const { handleChange, handleInsert, handleToggle, handleRemove } = this;
 
     return (
       <PageTemplate>
-        <TodoInput onChange={handleChange} onInsert={handleInsert} value={input}/> {/* PageTemplate's children */}
-        <TodoList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
+        <TodoInput
+          onChange={handleChange}
+          onInsert={handleInsert}
+          value={input}
+        />{' '}
+        {/* PageTemplate's children */}
+        <TodoList
+          todos={todos}
+          onToggle={handleToggle}
+          onRemove={handleRemove}
+        />
       </PageTemplate>
     );
   }
-
 }
 
 export default App;
--- todo-list2/src/components/TodoList/TodoList.js
+++ todo-list2/src/components/TodoList/TodoList.js
@@ -2,29 +2,25 @@ import React, { Component } from 'react';
 import TodoItem from '../TodoItem';
 
 class TodoList extends Component {
-
   shouldComponentUpdate(nextProps, nextState) {
-    return this.props.todos !== nextProps.todos;
+    const { todos } =this.props;
+    return todos !== nextProps.todos;
   }
+
   render() {
     const { todos, onToggle, onRemove } = this.props;
-    const todoList = todos.map(
-      todo => (
-        <TodoItem
-          key={todo.id}
-          done={todo.done}
-          onToggle={() => onToggle(todo.id)}
-          onRemove={() => onRemove(todo.id)}>
-          {todo.text}
-        </TodoItem>
-      )
-    );
+    const todoList = todos.map(todo => (
+      <TodoItem
+        key={todo.id}
+        done={todo.done}
+        onToggle={() => onToggle(todo.id)}
+        onRemove={() => onRemove(todo.id)}
+      >
+        {todo.text}
+      </TodoItem>
+    ));
 
-    return (
-      <div>
-        {todoList}
-      </div>
-    );
+    return <div>{todoList}</div>;
   }
 }
 
