7ed8d164492d6d8d659241f43ccdd15bab5f1bc4
WDever <alsrbtkfkd7@gmail.com>
2019-01-13 16:06:53 +0900

    fix

--- nasa-zpod/src/lib/api.js
+++ nasa-zpod/src/lib/api.js
@@ -2,5 +2,5 @@
 import axios from 'axios';
 
 export function getAPOD(date = '') { // date = '' === date가 없을시 공백 처리
-  return axios.get(`https://api.nasa.gov/planetary/apod?api_key=x5vPVqUkY0EFLlWcGcZJsnYNwquIt0hm0pwjEpsW&date=${date}`);
+  return axios.get(`https://api.nasa.gov/planetary/apod?api_key=x5vPVqUkY0EFLlWcGcZJsnYNwquIt0hm0pwjEpsW&date=${date}&hd=${true}`);
 }
