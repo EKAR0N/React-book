aca905cf95fa7a841d19ea1d3612300162b9c511
WDever <alsrbtkfkd7@gmail.com>
2019-02-07 11:21:12 +0900

    .

---
+++ typechain/index.ts
@@ -0,0 +1,11 @@
+const test = {
+  name: 'choi',
+  age: 18,
+  gender: 'male',
+};
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Your name is ${name}, Your age is ${age}, You are ${gender}`);
+};
+
+sayHi(test.name, test.age);
